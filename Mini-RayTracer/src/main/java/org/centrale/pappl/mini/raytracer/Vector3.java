e2ad50e5150140aa229a7188ab6a7e4be11b7ff3
Syned <synedday@gmail.com>
2019-10-14 15:00:42 +0200

    Vector3

--- Mini-RayTracer/src/main/java/org/centrale/pappl/mini/raytracer/Vector3.java
+++ Mini-RayTracer/src/main/java/org/centrale/pappl/mini/raytracer/Vector3.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.centrale.pappl.mini.raytracer;
 
-/**
- *
- * @author skiara
- */
 public class Vector3 {
-    
+    public double x;
+    public double y;
+    public double z;
+
+    public Vector3() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    public Vector3(double x) {
+        this.x = x;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    public Vector3(double x, double y) {
+        this.x = x;
+        this.y = y;
+        this.z = 0;
+    }
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3(Vector3 vector3) {
+        this.x = vector3.x;
+        this.y = vector3.y;
+        this.z = vector3.z;
+    }
+
+    public void add(Vector3 vector3) {
+        this.x += vector3.x;
+        this.y += vector3.y;
+        this.z += vector3.z;
+    }
+
+    public Vector3 substract(Vector3 vector3) {
+        return new Vector3(x - vector3.x, y - vector3.y, z - vector3.z);
+    }
+
+    public Vector3 normalize() {
+        double lenght = Math.sqrt(scalarProduct(this));
+        if(Math.abs(lenght) == 0) {
+            return new Vector3();
+        }
+        return new Vector3(x/lenght, y/lenght, z/lenght);
+    }
+
+    public double scalarProduct(Vector3 vector3) {
+        return x*vector3.x + y*vector3.y + z*vector3.z;
+    }
+
+    public Vector3 scalarMultiplication(double d) {
+        return new Vector3(x*d, y*d, z*d);
+    }
+
 }
