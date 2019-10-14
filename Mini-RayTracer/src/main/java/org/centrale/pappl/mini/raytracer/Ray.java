b100c002e7ea6b3d3469708a6e6d7b63d8f281b9
skiara <56543009+skiara@users.noreply.github.com>
2019-10-14 14:51:30 +0200

    ray

---
+++ Mini-RayTracer/src/main/java/org/centrale/pappl/mini/raytracer/Ray.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.pappl.mini.raytracer;
+
+/**
+ *
+ * @author skiara
+ */
+public class Ray {
+    //ATTRIBUTES
+    /**
+     * Ray's origin
+     */
+    private Vector3 origin;
+    
+    /**
+     * Ray's direction
+     */
+    private Vector3 direction;
+    
+    //CONSTRUCTORS
+    /**
+     * Constructor
+     * @param origin
+     * @param direction 
+     */
+    public Ray(Vector3 origin, Vector3 direction){
+        this.origin = origin;
+        this.direction = direction;
+    }
+    
+    /**
+     * Default constructor
+     */
+    public Ray(){
+        this.origin = new Vector3();
+        this.direction = new Vector3();
+    }
+
+    
+    //GETTERS AND SETTERS
+    public Vector3 getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Vector3 origin) {
+        this.origin = origin;
+    }
+
+    public Vector3 getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Vector3 direction) {
+        this.direction = direction;
+    }
+    
+}
