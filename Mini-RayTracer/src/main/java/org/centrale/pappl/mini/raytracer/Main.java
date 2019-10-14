70b9900763b4796c21f046b69523f274987ae415
skiara <56543009+skiara@users.noreply.github.com>
2019-10-14 15:38:16 +0200

    main

--- Mini-RayTracer/src/main/java/org/centrale/pappl/mini/raytracer/Main.java
+++ Mini-RayTracer/src/main/java/org/centrale/pappl/mini/raytracer/Main.java
@@ -12,5 +12,21 @@ package org.centrale.pappl.mini.raytracer;
 public class Main {
     public static void main(String[] args){
         
+        Scene scene = Scene.getScene();
+        
+        //Fill objects
+        //Fill lights
+        
+        
+        //For each pixel in raster
+            //create ray camera to pixel builtCameraRay()
+            //For each Object in objects
+                //Test for closest intersection: trace() 
+                //if true
+                    //fill pixel with object color at intersection
+                // else
+                    //fill pixel with background color
+        
+        
     }
 }
