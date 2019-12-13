8c3fb9d0cfd1fff97dca2ea83f956906aa4a6d58
skiara <56543009+skiara@users.noreply.github.com>
2019-12-13 18:07:16 +0100

    Commit2
    Javadoc update

--- Mini-RayTracer/src/main/java/org/centrale/pappl/mini/raytracer/Main.java
+++ Mini-RayTracer/src/main/java/org/centrale/pappl/mini/raytracer/Main.java
@@ -42,27 +42,27 @@ public class Main {
         // Fill objects
         
         Material material1 = new Material(0.3, 1, 1, 1);
+        Material material2 = new Material(0.3, 0.3, 1, 1);
         
-        SceneObject sceneObject2 = new Sphere(new Vector3(0, 0, -1200), 50);
+        SceneObject sceneObject2 = new Sphere(new Vector3(400, 0, -1500), 300);
         sceneObject2.setColor(new Vector3((float) 0x9F / 0xFF, (float) 0x3C / 0xFF, (float) 0x33 / 0xFF));
-        sceneObject2.setMaterial(material1);
-        //scene.addObject(sceneObject2);
+        sceneObject2.setMaterial(material2);
+        scene.addObject(sceneObject2);
 
-        SceneObject sceneObject3 = new Sphere(new Vector3(-300, 100, -1100), 50);
+        SceneObject sceneObject3 = new Sphere(new Vector3(500, 100, -1100), 50);
         sceneObject3.setColor(new Vector3((float) 0x6a / 0xFF, (float) 0x40 / 0xFF, (float) 0x9B / 0xFF));
         //sceneObject3.setColor(new Vector3(0, 0.9, 0.9)); // Bug: interaction avec lumière ambiente
-        sceneObject3.setMaterial(material1);
-        //scene.addObject(sceneObject3);
+        sceneObject3.setMaterial(material2);
+        scene.addObject(sceneObject3);
 
-        //SceneObject sceneObject5 = new TriangleMeshBox(new Vector3(-700, 150, -1300), new Vector3(-100, 400, -2000), new Vector3(1, -1, 0).normalized(), new Vector3(1, 1, 0).normalized(), Scene.UZ);
-        SceneObject sceneObject5 = new TriangleMeshBox(new Vector3(100, 300, -1500), new Vector3(-700, -300, -1200), new Vector3(0.87, 0, -0.5), Scene.UY, new Vector3(0.5, 0, 0.87));
+        SceneObject sceneObject5 = new TriangleMeshBox(new Vector3(-700, 200, -1300), new Vector3(-100, 400, -2000), new Vector3(1, -1, 0).normalized(), new Vector3(1, 1, 0).normalized(), Scene.UZ);
         sceneObject5.setColor(new Vector3((float) 0x73 / 0xFF, (float) 0xab / 0xFF, (float) 0x63 / 0xFF));
-        sceneObject5.setMaterial(material1);
+        sceneObject5.setMaterial(material2);
         scene.addObject(sceneObject5);
 
-        SceneObject sceneObject6 = new TriangleMeshBox(new Vector3(0,0,-1200), new Vector3(50, 50, -1150), new Vector3(1, 1, 0).normalized(), new Vector3(-1, 1, 0).normalized(), Scene.UZ);
+        SceneObject sceneObject6 = new TriangleMeshBox(new Vector3(-400, -200,-1200), new Vector3(-700, -400, -1500), Scene.UX, Scene.UY, Scene.UZ);
         sceneObject6.setColor(new Vector3((float) 0x9F / 0xFF, (float) 0x3C / 0xFF, (float) 0x33 / 0xFF));
-        sceneObject6.setMaterial(material1);
+        sceneObject6.setMaterial(material2);
         scene.addObject(sceneObject6);
 
         
@@ -76,8 +76,11 @@ public class Main {
         DirectionalLight dLight2 = new DirectionalLight(new Vector3(1, 1, -1), new Vector3(Color.white), 2);
 //        scene.addLight(dLight2);
 
-        PointLight pLight1 = new PointLight(new Vector3(Color.white), 1, new Vector3(100, 0, -1000));
+        PointLight pLight1 = new PointLight(new Vector3(Color.white), 1, new Vector3(300, 0, -1000));
         scene.addLight(pLight1);
+        
+        PointLight pLight2 = new PointLight(new Vector3(Color.white), 1, new Vector3(-600, 400, -1400));
+        scene.addLight(pLight2);
 
         // For each pixel in raster
         for (int i = 0; i < Raster.WIDTH; i++){
